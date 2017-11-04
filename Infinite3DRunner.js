0797c13f162d28509ebfb1fb1b991bee3b815a95
milo <haradra>
2017-11-04 20:26:05 +0000

    Add player object

--- Infinite3DRunner.js
+++ Infinite3DRunner.js
@@ -167,3 +167,17 @@ function handleKeyDown(keyEvent){
 		bounceValue=0.06;
 	}
 }
+function addHero(){
+	var sphereGeometry = new THREE.DodecahedronGeometry( heroRadius, 1);
+	var sphereMaterial = new THREE.MeshStandardMaterial( { color: 0xe5f2f2 ,shading:THREE.FlatShading} )
+	jumping=false;
+	heroSphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
+	heroSphere.receiveShadow = true;
+	heroSphere.castShadow=true;
+	scene.add( heroSphere );
+	heroSphere.position.y=heroBaseY;
+	heroSphere.position.z=4.8;
+	currentLane=middleLane;
+	heroSphere.position.x=currentLane;
+}
+
