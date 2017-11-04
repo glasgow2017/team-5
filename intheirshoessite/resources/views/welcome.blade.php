cf85fc64eef140ad8c550022fb0c472517f4de83
Hemang Kandwal <2285702k@student.gla.ac.uk>
2017-11-04 16:41:06 +0000

    #WhenYourGlobeRotates

--- intheirshoessite/resources/views/welcome.blade.php
+++ intheirshoessite/resources/views/welcome.blade.php
@@ -1,6 +1,13 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="http://www.webglearth.com/v2/api.js"></script>
+  
+    <style>
+    
+      html, body{padding: 0; margin: 0;}
+      </style>
     <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
         <style>
             html,body
@@ -11,9 +18,38 @@
         
     </head>
     <body style="text-align:center;">
+    
         <h1 style="font-size:3.5em;margin-top:10%;">
             In their shoes
         </h1>
         <img src="{{ URL::asset('img/front-page-girl.PNG') }}" style="margin-top:-3%;width:600px;margin-left:40px;"/>
-    </body>
+
+        <script>
+
+        function initialize(lat,lng,country) {
+                var earth = new WE.map('earth_div');
+                WE.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(earth);
+
+                var marker = WE.marker([lat,lng]).addTo(earth);
+                marker.bindPopup(country, {maxWidth: 150, closeButton: true}).openPopup();
+
+                earth.setView([lat, lng], 3);
+                }
+        
+            $(document).ready(function()
+            {
+                $.get("http://ip-api.com/json", function(response) {
+                    initialize(response.lat,response.lon,response.country);
+                });
+            });
+                
+
+    
+    </script> 
+
+      <div id="earth_div"></div>
+  
+
+</body>
+     
 </html>
