14476bf8e112fae8de13c03063546a7ed25ab793
Hemang Kandwal <2285702k@student.gla.ac.uk>
2017-11-04 13:52:32 +0000

    #FirstEver #nowthatisastart #getwithus #teamhood #hashtag

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <style>
+            html,body
+            {
+                font-family: 'Raleway', sans-serif;
+            }
+        </style>
+    </head>
+    <body>
+        <h4 style="text-align:center">
+            In their shoes
+        </h4>
+    </body>
+</html>
