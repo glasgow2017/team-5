c92af6aa71d63e7a00d4cdadcbdf778eb119fa55
milo <haradra>
2017-11-04 20:27:41 +0000

    Add lighting

--- Infinite3DRunner.js
+++ Infinite3DRunner.js
@@ -225,4 +225,18 @@ function addWorld(){
 	rollingGroundSphere.position.z=2;
 	addWorldTrees();
 }
+function addLight(){
+	var hemisphereLight = new THREE.HemisphereLight(0xfffafa,0x000000, .9)
+	scene.add(hemisphereLight);
+	sun = new THREE.DirectionalLight( 0xcdc1c5, 0.9);
+	sun.position.set( 12,6,-7 );
+	sun.castShadow = true;
+	scene.add(sun);
+	//Set up shadow properties for the sun light
+	sun.shadow.mapSize.width = 256;
+	sun.shadow.mapSize.height = 256;
+	sun.shadow.camera.near = 0.5;
+	sun.shadow.camera.far = 50 ;
+}
+
 
