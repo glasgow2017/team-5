7cb4d079e36441888fcd3897286911ce1568b63e
modelorona <angel.hristozov@gmail.com>
2017-11-05 04:39:39 +0000

    .innerHTML

--- intheirshoessite/resources/views/welcome.blade.php
+++ intheirshoessite/resources/views/welcome.blade.php
@@ -140,7 +140,7 @@
                                     // var whatToSend = '<h3> Your Country is Good, but do you know this one?</h3> ';
                                     initialize(response.lat,response.lon,response.country,htmlstr);
                                     $("#submit").on("click",function(){
-                                        console.log($("input[name='optradio']:checked").val());
+                                        console.log($("input[name='optradio']:checked").innerHTML);
                                     });
                                 }
                            }
