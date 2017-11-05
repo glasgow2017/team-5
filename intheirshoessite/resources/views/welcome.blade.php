f0c01be0567db406f24af6fdeebe388a2a903f58
modelorona <angel.hristozov@gmail.com>
2017-11-05 04:34:26 +0000

    Trying...

--- intheirshoessite/resources/views/welcome.blade.php
+++ intheirshoessite/resources/views/welcome.blade.php
@@ -137,12 +137,8 @@
                                                 </div><br><br>\
                                                 <button id="submit" style="margin-left:25%;">I am sure!</button>\
                                                 <script>\
-                                                $("#submit").on("click",function()\
-                                                {\
-                                                    console.log($("input[name=optradio]:checked").val());\
-                                                });\
-                                                </script>\
-                                                ';
+                                                $("#submit").on("click",function(){console.log($("input[name=optradio]:checked").val());});\
+                                                </script>';
                                       
                                     // var whatToSend = '<h3> Your Country is Good, but do you know this one?</h3> ';
                                     initialize(response.lat,response.lon,response.country,htmlstr);
