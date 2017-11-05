84e06f79ca1e64560bdcd85c3cd76a4a1d11338b
Michelle <michelle@Computer.local>
2017-11-05 04:56:24 +0000

    clean up of fetch_data, added some comments

--- fetch_data.py
+++ fetch_data.py
@@ -1,6 +1,11 @@
 import urllib, json
 import pprint
 
+# IDs for countries and data types retrieved from the database
+countries = ["ID", "IN", "RW", "GB"]
+indicators = ["SE.ADT.LITR.ZS", "SG.LAW.CHMR", "SH.STA.MMRT", "SL.EMP.1524.SP.FE.NE.ZS", "SP.MTR.1519.ZS"]
+
+# retrieves the worldbank data for given country and indicator as json.
 def fetch_data(indicator, country):
 	url = "http://api.worldbank.org/countries/" + country
 	url += "/indicators/" + indicator + "?format=json"
@@ -8,10 +13,7 @@ def fetch_data(indicator, country):
 	json_file = json.loads(response.read())
 	return json_file
 
-countries = ["ID", "IN", "RW", "GB"]
-indicators = ["SE.ADT.LITR.ZS", "SG.LAW.CHMR", "SH.STA.MMRT", "SL.EMP.1524.SP.FE.NE.ZS", "SP.MTR.1519.ZS"]
-
-
+# load all required data from worldbank database, return as array of json.
 def load_all_data():
 	datas = []
 
@@ -20,7 +22,3 @@ def load_all_data():
 			datas.append(fetch_data(i, c))
 
 	return datas
-
-pp = pprint.PrettyPrinter(depth=6)
-pp.pprint(load_all_data())
-
